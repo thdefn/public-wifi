f5cb7b6a45adef3647c68661ed3ac22d34500849
haden <80521474+thdefn@users.noreply.github.com>
2023-02-12 12:20:35 +0900

    url 수정

--- src/main/java/com/zerobase/publicwifi/HistoryServlet.java
+++ src/main/java/com/zerobase/publicwifi/HistoryServlet.java
@@ -18,13 +18,15 @@ public class HistoryServlet extends HttpServlet {
     //조회 히스토리 삭제 기능
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         int id = Integer.parseInt(request.getParameter("hist_id"));
+
+        //DB에서 삭제 처리하기
         WifiService wifiService = new WifiService();
         wifiService.deleteHistory(id);
 
         response.setCharacterEncoding("UTF-8");
         request.setAttribute("id",id);
 
-        //해당 table row 삭제 처리를 위해 다른 jsp 내려줌
+        //table row 삭제 처리를 위해 다른 jsp 내려줌
         ServletContext context = getServletContext();
         RequestDispatcher dispatcher = context.getRequestDispatcher("/result.jsp");
         dispatcher.forward(request, response);
